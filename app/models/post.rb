34b264ed659ee2a5dc6c003306e1eb7c797ebce0
nazik23 <nazar.kononets23@gmail.com>
2018-04-23 09:23:39 +0300

    fixed add_comment button

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,5 @@
 class Post < ApplicationRecord
 	acts_as_taggable_on :tags
-	acts_as_commentable
 	validates :body, presence: true, length:
 	       	{ maximum: 1000, too_long: "%{count} characters is maximum allowed."}
 	validates :title, length: {in: 5..100 }, allow_blank: true
